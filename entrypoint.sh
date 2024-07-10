9828993238966354dbbc436d3cee41b801da9eba
Earion <54678154+earion68@users.noreply.github.com>
2024-07-10 12:35:38 +0200

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+apk add poetry
+apk add sqlite
+rm songs.db
+sqlite3 aFile.db "create table aTable(field1 int); drop table aTable;"
+poetry install
+poetry run python index.py
+poetry run flask run --host=0.0.0.0
